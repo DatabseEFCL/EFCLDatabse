dfa87dc96e02f6c5a926c276a1a289e27434401f
Edmonton Federation of Community Leagues <87148095+DatabseEFCL@users.noreply.github.com>
2021-07-12 16:54:41 -0600

    Update EFCL.py

--- EFCL.py
+++ EFCL.py
@@ -12,6 +12,7 @@ st.sidebar.subheader("Visualization Settings")
 #File upload
 file= st.sidebar.file_uploader(label= "Upload your csv or excel file.", type= ['csv', 'xlsx'])
 
+'''
 global df
 if file is not None:
     print(file)
@@ -26,3 +27,4 @@ try:
 except Exception as e:
     print(e)
     str.write("Please upload file to the application.")
+'''
