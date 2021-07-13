ec66c3b7db3623201bcebc1c3d3014a5fd8ce02b
Edmonton Federation of Community Leagues <87148095+DatabseEFCL@users.noreply.github.com>
2021-07-13 16:17:02 -0600

    Update EFCL.py

--- EFCL.py
+++ EFCL.py
@@ -9,40 +9,37 @@ import numpy as np
 
     
 
-#def filtering(df):
-        
 
-if __name__=="__main__":
 
 #title
-    st.title(""" EFCL CLOG Database """)
-    st.text("Please upload file to the application.")
+st.title(""" EFCL CLOG Database """)
+st.text("Please upload file to the application.")
 
-    
-    #Sidebar
-    st.sidebar.subheader("Visualization Settings")
 
-    #file upload
-    file= st.sidebar.file_uploader(label= "Upload your csv file.", type= ['csv'])
+#Sidebar
+st.sidebar.subheader("Visualization Settings")
 
-    global df 
-    
-    if file is not None:
-            try: 
-                df = pd.read_csv(file)
-                st.markdown('Your csv file has been uploaded !')
-                
-            except Exception as e: 
-                print(e)
-                
-    df = pd.read_csv(file)
-    Com = df['Community League'].drop_duplicates()
-    Com_choice = st.sidebar.selectbox('Select your Community League:', Com)
-    Program = df["Program"].loc[df["Community League"] == Com_choice]
-    Prog_choice = st.sidebar.selectbox('Select your Program:', Program)
-    Delivery = df["Delivery"].loc[df["Program"] == Prog_choice]
-    while Program == True:
-        st.table(Program) 
-    while Delivery == True:
-        st.table(Delivery)       
+#file upload
+file= st.sidebar.file_uploader(label= "Upload your csv file.", type= ['csv'])
+
+global df 
+
+if file is not None:
+        try: 
+            df = pd.read_csv(file)
+            st.markdown('Your csv file has been uploaded !')
+
+        except Exception as e: 
+            print(e)
+
+df = pd.read_csv(file)
+Com = df['Community League'].drop_duplicates()
+Com_choice = st.sidebar.selectbox('Select your Community League:', Com)
+Program = df["Program"].loc[df["Community League"] == Com_choice]
+Prog_choice = st.sidebar.selectbox('Select your Program:', Program)
+Delivery = df["Delivery"].loc[df["Program"] == Prog_choice]
+while Program == True:
+    st.table(Program) 
+while Delivery == True:
+    st.table(Delivery)       
 
