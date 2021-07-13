99c3e46439064fb66e365d7543fb57d5898bf859
Edmonton Federation of Community Leagues <87148095+DatabseEFCL@users.noreply.github.com>
2021-07-13 13:02:20 -0600

    Update EFCL.py

--- EFCL.py
+++ EFCL.py
@@ -21,14 +21,19 @@ def file():
         try: 
             df = pd.read_csv(file)
             
-        except Exception as e:
+        except Exception as e: 
             print(e)
             df= pd.read_excel(file)
     try:
-        st.write(df)
+        filters()
     except Exception as e:
         print(e)
         st.write("Please upload file to the application.")
 
+def filters():
+    Community= df['Community League'].drop_duplicates()
+    ComChoice= st.sidebar.selectbox('Select your Community League:', Community)
+    Program= df["Program"].loc[df["Community League","Delivery"]== ComChoice]
+    st.write(df)
 if __name__=="__main__":
     file()
