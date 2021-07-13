44275e2bc2ff8ce4a2d1a096da40b826b6749dbe
Edmonton Federation of Community Leagues <87148095+DatabseEFCL@users.noreply.github.com>
2021-07-13 16:16:22 -0600

    Update EFCL.py

--- EFCL.py
+++ EFCL.py
@@ -26,6 +26,7 @@ if __name__=="__main__":
     file= st.sidebar.file_uploader(label= "Upload your csv file.", type= ['csv'])
 
     global df 
+    
     if file is not None:
             try: 
                 df = pd.read_csv(file)
@@ -34,17 +35,14 @@ if __name__=="__main__":
             except Exception as e: 
                 print(e)
                 
-    else: 
-    
-        df = pd.read_csv(file)
-
-        Com = df['Community League'].drop_duplicates()
-        Com_choice = st.sidebar.selectbox('Select your Community League:', Com)
-        Program = df["Program"].loc[df["Community League"] == Com_choice]
-        Prog_choice = st.sidebar.selectbox('Select your Program:', Program)
-        Delivery = df["Delivery"].loc[df["Program"] == Prog_choice]
-        while Program == True:
-            st.table(Program) 
-        while Delivery == True:
-            st.table(Delivery)       
+    df = pd.read_csv(file)
+    Com = df['Community League'].drop_duplicates()
+    Com_choice = st.sidebar.selectbox('Select your Community League:', Com)
+    Program = df["Program"].loc[df["Community League"] == Com_choice]
+    Prog_choice = st.sidebar.selectbox('Select your Program:', Program)
+    Delivery = df["Delivery"].loc[df["Program"] == Prog_choice]
+    while Program == True:
+        st.table(Program) 
+    while Delivery == True:
+        st.table(Delivery)       
 
