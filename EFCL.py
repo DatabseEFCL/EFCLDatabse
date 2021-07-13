9aac5160b1c20bd8ffdc3b4ebedaf7cb895cec81
Edmonton Federation of Community Leagues <87148095+DatabseEFCL@users.noreply.github.com>
2021-07-12 19:20:54 -0600

    Update EFCL.py

--- EFCL.py
+++ EFCL.py
@@ -11,29 +11,25 @@ st.sidebar.subheader("Visualization Settings")
 #File upload
 file= st.sidebar.file_uploader(label= "Upload your csv or excel file.", type= ['csv', 'xlsx'])
 
-def upload_file():
         
-    #checking which file is uploaded 
-    global df
+#checking which file is uploaded 
+global df
 
-    if file is not None:
-        print(file)
-        try: 
-            df = pd.read_csv(file)
-        except Exception as e:
-            print(e)
-            df= pd.read_excel(file)
-    try:
-        st.write(df)
+if file is not None:
+    print(file)
+    try: 
+        df = pd.read_csv(file)
     except Exception as e:
         print(e)
-        st.write("Please upload file to the application.")
+        df= pd.read_excel(file)
+try:
+    st.write(df)
+except Exception as e:
+    print(e)
+    st.write("Please upload file to the application.")
 
-    return file 
 
-@st.cache
-def filter_data(df):
-    ComL= df['Community League']. drop_duplicates()
-    ComChoice= st.sidebar.selectbox('Select your Community League', ComL)
-    Prog= df["Program"].loc[df["Community League']= ComChoice]
-    Progchoice= st.sidebar.selectbox('Select your Program',Prog)
+ComL= df['Community League']. drop_duplicates()
+ComChoice= st.sidebar.selectbox('Select your Community League: ', ComL)
+Prog= df["Program"].loc[df["Community League"]== ComChoice]
+ProgChoice= st.sidebar.selectbox('Select your Program', Prog)
