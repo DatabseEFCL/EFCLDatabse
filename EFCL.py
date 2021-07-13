b6c68ec7ef325739b1235c36298023630b160027
Edmonton Federation of Community Leagues <87148095+DatabseEFCL@users.noreply.github.com>
2021-07-13 16:13:45 -0600

    Update EFCL.py

--- EFCL.py
+++ EFCL.py
@@ -1,4 +1,4 @@
-from altair.vegalite.v4.schema.core import CompositeMark
+
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -6,21 +6,6 @@ import numpy as np
 
 
 
-@st.cache
-def filter(file):
-    path = r'file'
-    return pd.read_csv(path)
-    df = filter(file)
-
-    Com = df['Community League'].drop_duplicates()
-    Com_choice = st.sidebar.selectbox('Select your Community League:', Com)
-    Program = df["Program"].loc[df["Community League"] == Com_choice]
-    Prog_choice = st.sidebar.selectbox('Select your Program:', Program)
-    Delivery = df["Delivery"].loc[df["Program"] == Prog_choice]
-    while Program == True:
-        st.table(Program) 
-    while Delivery == True:
-        st.table(Delivery)       
 
     
 
@@ -50,5 +35,16 @@ if __name__=="__main__":
                 print(e)
                 
     else: 
-        filter(file)
+    
+        df = pd.read_csv(file)
+
+        Com = df['Community League'].drop_duplicates()
+        Com_choice = st.sidebar.selectbox('Select your Community League:', Com)
+        Program = df["Program"].loc[df["Community League"] == Com_choice]
+        Prog_choice = st.sidebar.selectbox('Select your Program:', Program)
+        Delivery = df["Delivery"].loc[df["Program"] == Prog_choice]
+        while Program == True:
+            st.table(Program) 
+        while Delivery == True:
+            st.table(Delivery)       
 
