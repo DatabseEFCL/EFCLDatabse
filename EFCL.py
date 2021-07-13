e9834194196d7d2019eb7198981f15accb496dc7
Edmonton Federation of Community Leagues <87148095+DatabseEFCL@users.noreply.github.com>
2021-07-12 20:03:23 -0600

    Update EFCL.py

--- EFCL.py
+++ EFCL.py
@@ -1,46 +1,40 @@
 import streamlit as st
 import pandas as pd
 
+def file():
 
-#title
-st.title(""" EFCL CLOG Database """)
+    #title
+    st.title(""" EFCL CLOG Database """)
 
-#Sidebar
-st.sidebar.subheader("Visualization Settings")
+    #Sidebar
+    st.sidebar.subheader("Visualization Settings")
 
-#File upload
-file= st.sidebar.file_uploader(label= "Upload your csv or excel file.", type= ['csv', 'xlsx'])
+    #File upload
+    file= st.sidebar.file_uploader(label= "Upload your csv or excel file.", type= ['csv', 'xlsx'])
 
-        
-#checking which file is uploaded 
-def file_upload():
-        
+    return file      
+    #checking which file is uploaded 
+
+def check(file):
+
     global df
 
     if file is not None:
         print(file)
         try: 
             df = pd.read_csv(file)
+            
         except Exception as e:
             print(e)
             df= pd.read_excel(file)
     try:
-        st.write(df)
+        Community= df['Community League'].drop_duplicates()
+        ComChoice= st.sidebar.selectbox('Select your Community League:', Community)
+        Program= df["Program"].loc[df["Community League","Delivery"]== ComChoice]
+        st.write(Program)
+        
     except Exception as e:
         print(e)
         st.write("Please upload file to the application.")
     return df
-@st.cache
-def main(df):
 
-    path = df
-    return path
-    df = main()
-
-    ComL= df['Community League']. drop_duplicates()
-    ComChoice= st.sidebar.selectbox('Select your Community League: ', ComL)
-    Prog= df["Program"].loc[df["Community League"]== ComChoice]
-    ProgChoice= st.sidebar.selectbox('Select your Program', Prog)
-
-if __name__ == "__main__":
-    main(df)
