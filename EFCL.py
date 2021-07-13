e4e7d851f3d6ea9135d4fde6c5b3aecb5fc25d86
Edmonton Federation of Community Leagues <87148095+DatabseEFCL@users.noreply.github.com>
2021-07-13 16:07:01 -0600

    Update EFCL.py

--- EFCL.py
+++ EFCL.py
@@ -1,37 +1,54 @@
+from altair.vegalite.v4.schema.core import CompositeMark
 import streamlit as st
 import pandas as pd
 import numpy as np
 
-def file():
 
-    #title
+
+
+@st.cache
+def filter(file):
+    path = r'file'
+    return pd.read_csv(path)
+    df = filter(file)
+
+    Com = df['Community League'].drop_duplicates()
+    Com_choice = st.sidebar.selectbox('Select your Community League:', Com)
+    Program = df["Program"].loc[df["Community League"] == Com_choice]
+    Prog_choice = st.sidebar.selectbox('Select your Program:', Program)
+    Delivery = df["Delivery"].loc[df["Program"] == Prog_choice]
+    while Program == True:
+        st.table(Program) 
+    while Delivery == True:
+        st.table(Delivery)       
+
+    
+
+#def filtering(df):
+        
+
+if __name__=="__main__":
+
+#title
     st.title(""" EFCL CLOG Database """)
-    st.write("Please upload file to the application.")
+    st.text("Please upload file to the application.")
 
-
+    
     #Sidebar
     st.sidebar.subheader("Visualization Settings")
 
-    #File upload
-    file= st.sidebar.file_uploader(label= "Upload your csv or excel file.", type= ['csv', 'xlsx'])
+    #file upload
+    file= st.sidebar.file_uploader(label= "Upload your csv file.", type= ['csv'])
 
-   
-
-    global df
-
+    global df 
     if file is not None:
-        print(file)
-        try: 
-            df = pd.read_csv(file)
-            st.markdown('Your csv file has been uploaded !')
-        except Exception as e: 
-            print(e)
-            df= pd.read_excel(file)
-            st.markdown('Your excel file has been uploaded !')
+            try: 
+                df = pd.read_csv(file)
+                st.markdown('Your csv file has been uploaded !')
+                
+            except Exception as e: 
+                print(e)
+                
+    else: 
+        filter(file)
 
-    
-        
-
-
-if __name__=="__main__":
-    file()
