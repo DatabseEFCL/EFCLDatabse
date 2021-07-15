8d8b9d3521bd53c01b80bf19c4338afd14cafe8a
Edmonton Federation of Community Leagues <87148095+DatabseEFCL@users.noreply.github.com>
2021-07-14 20:20:41 -0600

    Update EFCL.py

--- EFCL.py
+++ EFCL.py
@@ -1,6 +1,24 @@
 import streamlit as st
 import pandas as pd
+from streamlit.uploaded_file_manager import UploadedFile
 
+#title
 st.title("EFCL CLOG DATABASE")
 
+#sidebar title
 st.sidebar.header("Data Visualization Settings")
+
+#setup file upload
+uploaded_file= st.sidebar.file_uploader(label="Upload your CSV or Excel file.", type = ['csv','xlsx'])
+
+global df
+if uploaded_file is not None:
+    try:
+        df= pd.read_csv(uploaded_file)
+    except Exception as e:
+        print(e)
+        df= pd.read_excel(uploaded_file)
+try:
+    st.write(df)
+except  Exception as e:
+    str.write("Please upload file to application")
