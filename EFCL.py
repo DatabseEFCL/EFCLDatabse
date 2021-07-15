9fc396e7aa4050edb94a6bb6644d403df30610b8
Edmonton Federation of Community Leagues <87148095+DatabseEFCL@users.noreply.github.com>
2021-07-15 16:01:48 -0600

    Update EFCL.py

--- EFCL.py
+++ EFCL.py
@@ -19,13 +19,13 @@ def loadData(file_uploaded):
     st.dataframe(df,3000,500)
     return df
 
-if file_uploaded:
+while file_uploaded:
      temp= loadData(file_uploaded)
      Qst= st.selectbox("Choose the field you want to search by:",list(temp.head()),key = "1")
 
      if Qst == "Community League":
          Com=temp['Community League'].drop_duplicates()
          Com_choice= st.selectbox("Select the Community League:",list(Com),key = "2")
-         Program= temp.loc[(temp['Program']== Com_choice) & (temp['Delivery']== Com_choice) ]
+         Program= temp.loc[temp['Program']== Com_choice]
          st.table(Program)
       
