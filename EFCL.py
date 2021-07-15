9ad83fe9afdbf6da56ba0e5a0d1e987768e4b679
Edmonton Federation of Community Leagues <87148095+DatabseEFCL@users.noreply.github.com>
2021-07-14 18:28:56 -0600

    Update EFCL.py

--- EFCL.py
+++ EFCL.py
@@ -1,3 +1,4 @@
+from packaging.version import LegacyCmpKey
 import streamlit as st
 import pandas as pd
 
@@ -16,20 +17,25 @@ def loadData(file_uploaded):
 
 #title
 st.title(""" EFCL CLOG Database """)
-
+st.text("Please upload your csv file and select topic you want to search by.")
 #Sidebar
 st.sidebar.subheader("Visualization Settings")
 
 #File upload
 file_uploaded= st.sidebar.file_uploader(label= "Upload your csv file.", type= ['csv'], key ='file_uploader')
+League= st.checkbox("Search by Community League",key='1')
+Prog= st.checkbox("Search by Program",key='2')
 
-if file_uploaded is not None:
+st.write(League)
+st.write(Prog)
+
+while file_uploaded is not None:
     df= loadData(file_uploaded)
-
+'''
     Community= df['Community League'].drop_duplicates()
-    ComChoice= st.selectbox('Select your Community League:', Community, key = None)
+    ComChoice= st.sidebar.selectbox('Select your Community League:', Community, key = '1')
     Program= df["Program"].loc[df["Community League"]== ComChoice]
     st.write(Program)
 
-
+'''
     
