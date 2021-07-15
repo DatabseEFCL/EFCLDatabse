055f3663abf3bf51f93a9aa4bc673b7a1e6a3cc2
Edmonton Federation of Community Leagues <87148095+DatabseEFCL@users.noreply.github.com>
2021-07-14 19:55:47 -0600

    Update EFCL.py

--- EFCL.py
+++ EFCL.py
@@ -22,23 +22,13 @@ st.sidebar.subheader("Visualization Settings")
 
 #File upload
 file_uploaded= st.sidebar.file_uploader(label= "Upload your csv file.", type= ['csv'])
-League= st.checkbox("Search by Community League",key='1')
-Prog= st.checkbox("Search by Program",key='2')
 
-st.write(League)
-st.write(Prog)
 
 while file_uploaded is not None:
     df= loadData(file_uploaded)
     while file_uploaded:
-        if League:
             Community= df['Community League'].drop_duplicates()
             ComChoice= st.sidebar.selectbox('Select your Community League:', Community, )
             Program= df["Program"].loc[df["Community League"] & df["Delivery"].loc[df["Community League"]== ComChoice]]
             st.table(Program)
-        if Prog:
-            Community= df['Program']
-            ComChoice= st.sidebar.selectbox('Select your Community League:', Community, key = '1')
-            Delivery= df["Delivery"].loc[df["Community League"]== ComChoice]
-            st.table(Delivery)
-
+       
