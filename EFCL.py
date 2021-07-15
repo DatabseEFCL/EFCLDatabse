4938ecee41f8caa200669a7efe0a5ce27646a9e6
Edmonton Federation of Community Leagues <87148095+DatabseEFCL@users.noreply.github.com>
2021-07-14 20:07:53 -0600

    Update EFCL.py

--- EFCL.py
+++ EFCL.py
@@ -1,32 +1,4 @@
-
 import streamlit as st
 import pandas as pd
 
-
-
-@st.cache(allow_output_mutation=True)
-def loadData(file_uploaded):
-    df = pd.read_csv(file_uploaded, encoding='unicode_escape')
-    st.dataframe(df,3000,500)
-    return df 
-
-    
-
-
-
-#title
-st.title(""" EFCL CLOG Database """)
-st.text("Please upload your csv file, then select topic you want to search by.")
-#Sidebar
-st.sidebar.subheader("Visualization Settings")
-
-#File upload
-file_uploaded= st.sidebar.file_uploader(label= "Upload your csv file.", type= ['csv'])
-
-while file_uploaded is not None:
-    df= loadData(file_uploaded)
-   
-    Community= df['Community League'].drop_duplicates()
-    ComChoice= st.sidebar.selectbox('Select your Community League:', Community, )
-    Program= df["Program"].loc[df["Community League"] & df["Delivery"].loc[df["Community League"]== ComChoice]]
-    st.table(Program)
+st.title("EFCL CLOG DATABASE")
