5aed43ae0fcfeeadf8384d458bcce10cdc469c75
Edmonton Federation of Community Leagues <87148095+DatabseEFCL@users.noreply.github.com>
2021-07-14 20:55:14 -0600

    Update EFCL.py

--- EFCL.py
+++ EFCL.py
@@ -7,19 +7,28 @@ st.title("EFCL CLOG DATABASE")
 
 #sidebar title
 st.sidebar.header("Data Visualization Settings")
-
 #setup file upload
 uploaded_file= st.sidebar.file_uploader(label="Upload your CSV or Excel file.", type = ['csv','xlsx'])
 
-global df
-while uploaded_file is not None:
-    try:
-        df= pd.read_csv(uploaded_file)
-    except Exception as e:
-        print(e)
-        df= pd.read_excel(uploaded_file)
+def file():
 
-try:
-    st.write(df)
-except Exception as e:
-    print(e)
+    global df
+    if uploaded_file is not None:
+        try:
+            df= pd.read_csv(uploaded_file)
+        except Exception as e:
+            print(e)
+            df= pd.read_excel(uploaded_file)
+        finally:
+            str.write("Please upload file to application")
+    
+    try:
+        st.write(df)
+    except Exception as e:
+        str.write(e)
+        st.write("The uploaded file is incorrect")
+    finally:
+        file()
+        
+def csv_file():
+    
