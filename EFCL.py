06bee95467cb5eea27ea00fa685fe963ff3ed3ae
Edmonton Federation of Community Leagues <87148095+DatabseEFCL@users.noreply.github.com>
2021-07-15 16:00:19 -0600

    Update EFCL.py

--- EFCL.py
+++ EFCL.py
@@ -12,14 +12,14 @@ st.sidebar.subheader("Visualization Settings")
 file_uploaded= st.sidebar.file_uploader(label= "Upload your csv file.", type= ['csv'])
 
 
-@st.cache(allow_output_mutation=True)
+@st.cache(suppress_st_warning=True,allow_output_mutation=True)
 def loadData(file_uploaded):
     st.write("Your file has been uploaded !")
     df = pd.read_csv(file_uploaded, encoding='unicode_escape')
     st.dataframe(df,3000,500)
     return df
 
-while file_uploaded:
+if file_uploaded:
      temp= loadData(file_uploaded)
      Qst= st.selectbox("Choose the field you want to search by:",list(temp.head()),key = "1")
 
@@ -28,3 +28,4 @@ while file_uploaded:
          Com_choice= st.selectbox("Select the Community League:",list(Com),key = "2")
          Program= temp.loc[(temp['Program']== Com_choice) & (temp['Delivery']== Com_choice) ]
          st.table(Program)
+      
