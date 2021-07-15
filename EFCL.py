eeb95a7e382c594fb7d483e1f357b6496c27ea48
Edmonton Federation of Community Leagues <87148095+DatabseEFCL@users.noreply.github.com>
2021-07-14 19:28:55 -0600

    Update EFCL.py

--- EFCL.py
+++ EFCL.py
@@ -6,8 +6,7 @@ import pandas as pd
 
 @st.cache(allow_output_mutation=True)
 def loadData(file_uploaded):
-    df = pd.read_csv(file_uploaded, encoding='utf-8', nrows=1552)
-    df.columns = ['Community League', 'Program', 'Delivery']
+    df = pd.read_csv(file_uploaded, encoding='unicode_escape')
     st.dataframe(df,3000,500)
     return df 
 
@@ -17,12 +16,12 @@ def loadData(file_uploaded):
 
 #title
 st.title(""" EFCL CLOG Database """)
-st.text("Please upload your csv file and select topic you want to search by.")
+st.text("Please upload your csv file, then select topic you want to search by.")
 #Sidebar
 st.sidebar.subheader("Visualization Settings")
 
 #File upload
-file_uploaded= st.sidebar.file_uploader(label= "Upload your csv file.", type= ['csv'], key ='file_uploader')
+file_uploaded= st.sidebar.file_uploader(label= "Upload your csv file.", type= ['csv'], key ='0')
 League= st.checkbox("Search by Community League",key='1')
 Prog= st.checkbox("Search by Program",key='2')
 
@@ -31,11 +30,15 @@ st.write(Prog)
 
 while file_uploaded is not None:
     df= loadData(file_uploaded)
-'''
-    Community= df['Community League'].drop_duplicates()
-    ComChoice= st.sidebar.selectbox('Select your Community League:', Community, key = '1')
-    Program= df["Program"].loc[df["Community League"]== ComChoice]
-    st.write(Program)
+    while file_uploaded:
+        if League:
+            Community= df['Community League'].drop_duplicates()
+            ComChoice= st.selectbox('Select your Community League:', Community, key = '1')
+            Program= df["Program"].loc[df["Community League"] & df["Delivery"].loc[df["Community League"]== ComChoice]]
+            st.write(Program)
+        if Prog:
+            Community= df['Program']
+            ComChoice= st.selectbox('Select your Community League:', Community, key = '1')
+            Delivery= df["Delivery"].loc[df["Community League"]== ComChoice]
+            st.write(Delivery)
 
-'''
-    
