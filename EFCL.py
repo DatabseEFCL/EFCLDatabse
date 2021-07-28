d0cfdada6b843c8fe18226d696402aa0accf5ce3
Edmonton Federation of Community Leagues <87148095+DatabseEFCL@users.noreply.github.com>
2021-07-28 14:24:47 -0600

    Update EFCL.py

--- EFCL.py
+++ EFCL.py
@@ -43,7 +43,7 @@ if file_uploaded:
             Program = df["Program"].drop_duplicates().sort_values()
             Program_ch= st.selectbox("Select the Program:",list(Program),key = '3')
             Community= df["Community League"].loc[df['Program']== Program_ch].drop_duplicates().sort_values()
-            Delivery = df["Delivery"].loc[df['Program']== Program_ch]
+            Delivery = df["Delivery"].loc[df['Program']== Program_ch].drop_duplicates().sort_values()
             st.table(pd.concat([Community, Delivery], axis=1))
     
     if Qst == "Delivery":
