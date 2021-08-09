e6e2d8a1b1d2ab873dead277bd506f23a6011768
Edmonton Federation of Community Leagues <87148095+DatabseEFCL@users.noreply.github.com>
2021-08-09 15:06:36 -0600

    Update EFCL.py

--- EFCL.py
+++ EFCL.py
@@ -59,4 +59,4 @@ if __name__== "__main__":
         if st.button("Map"):
                 pass
         else:
-                st.markdown("<h2 style='text-align: right; color:#FF0000 ;'>There is no uploaded CSV file.</h1>", unsafe_allow_html=True)
+                st.markdown("<h2 style='text-align: left; color:#FF0000 ;'>There is no uploaded CSV file.</h1>", unsafe_allow_html=True)
