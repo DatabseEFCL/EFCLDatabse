8e2129cd604ca7d175b2034e3f53354aafe84f2f
Edmonton Federation of Community Leagues <87148095+DatabseEFCL@users.noreply.github.com>
2021-08-09 14:46:29 -0600

    Update EFCL.py

--- EFCL.py
+++ EFCL.py
@@ -9,26 +9,28 @@ st.text("Please upload your csv file, then select topic you want to search by.")
 st.sidebar.subheader("Visualization Settings")
 
 #File upload
-file_uploaded= st.sidebar.file_uploader(label= "Upload your csv or excel file.", type= ['csv'])
+file_uploaded= st.sidebar.file_uploader(label= "Upload your csv file.", type= ['csv'])
 
 
 @st.cache(suppress_st_warning=True,allow_output_mutation=True)
 def loadData(file_uploaded):
-    global df
+
+    st.write("Your file has been uploaded !")
     st.write("CAUTION: DO NOT select the option 'nan', it will cause a bug and you will have to refreash the page and insert the csv file again.")
+
     df = pd.read_csv(file_uploaded, encoding='unicode_escape')
-    
     
     return df
 
 
 if __name__== "__main__":
+        loadData(file_uploaded)
+        if file_uploaded:
 
-        if st.button("Access Database"):
-                if file_uploaded is not None:
-                        df= loadData(file_uploaded)
-                        st.write("CAUTION: DO NOT select the option 'nan', it will cause a bug and you will have to refreash the page and insert the csv file again.")
-                        Qst= st.selectbox("Choose the field you want to search by:",list(df.head()),key = "1")
+                df= loadData(file_uploaded)
+                st.write("Your file has been uploaded !")
+                st.write("CAUTION: DO NOT select the option 'nan', it will cause a bug and you will have to refreash the page and insert the csv file again.")
+                Qst= st.selectbox("Choose the field you want to search by:",list(df.head()),key = "1")
                 
                         
                 if Qst == "Community League":
@@ -37,15 +39,15 @@ if __name__== "__main__":
                         Program= df["Program"].loc[df['Community League']== Com_choice] 
                         Delivery = df["Delivery"].loc[df['Community League']== Com_choice]
                         st.table(pd.concat([Program, Delivery], axis=1))
-
-
+                        
+                        
                 if Qst == "Program":
                         Program = df["Program"].drop_duplicates().sort_values()
                         Program_ch= st.selectbox("Select the Program:",list(Program),key = '3')
                         Community= df["Community League"].loc[df['Program']== Program_ch]
                         Delivery = df["Delivery"].loc[df['Program']== Program_ch]
                         st.table(pd.concat([Community, Delivery], axis=1))
-
+                
                 if Qst == "Delivery":
                         Delivery= df["Delivery"].drop_duplicates().sort_values()
                         col1,col2=st.beta_columns(2)
@@ -54,8 +56,3 @@ if __name__== "__main__":
                         Program= P_lower.loc[df['Delivery']== D_ch] 
                         Community= df["Community League"].loc[df['Delivery']== D_ch]
                         st.table(pd.concat([Program, Community], axis=1))
-                else:
-                        st.write("There is no csv file uploaded.")
-        if st.button("Access Map"):
-                pass
-
