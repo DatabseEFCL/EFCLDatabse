560cd88fddcd3e9d5e91d8150962e795525b5b06
Edmonton Federation of Community Leagues <87148095+DatabseEFCL@users.noreply.github.com>
2021-08-11 19:05:49 -0600

    Update EFCL.py

--- EFCL.py
+++ EFCL.py
@@ -79,5 +79,7 @@ if __name__== "__main__":
                 map(file_uploaded2)
                 df= map(file_uploaded2)
                 if file_uploaded2 is not None:
-                        
                         League= st.selectbox("Select Community League :", list(df["Community League"]),key='z')
+                        MailAd= df["Mailing Address"].loc[df['Community League']== League]
+                        StreetAd= df["Street Address"].loc[df['Community League']== League]
+                        st.selectbox(MailAd,StreetAd, key='y')
