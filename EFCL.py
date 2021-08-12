46a21e9723d52f4b58526db328ac299282822b36
Edmonton Federation of Community Leagues <87148095+DatabseEFCL@users.noreply.github.com>
2021-08-11 19:44:23 -0600

    Update EFCL.py

--- EFCL.py
+++ EFCL.py
@@ -86,6 +86,6 @@ if __name__== "__main__":
                         StreetAd= df["Street Address"].loc[df['Community League']== ratio]
                         
                         if ratio == 'Street Address':
-                                st.selectbox(list(df["StreetAd"]),key='l')
+                                st.selectbox(list(df["Street Address"]),key='l')
                         if ratio == "Mailing Address":
-                                st.selectbox(list(df["MailAd"]),key='m')
+                                st.selectbox(list(df["Mailing Address"]),key='m')
