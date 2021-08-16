87a95d385836836133d818ce5a24798b26c49712
Edmonton Federation of Community Leagues <87148095+DatabseEFCL@users.noreply.github.com>
2021-08-16 13:00:40 -0600

    Update EFCL.py

--- EFCL.py
+++ EFCL.py
@@ -42,8 +42,8 @@ def database():
         if file_uploaded:
            
             df= loadData(file_uploaded)
-            st.write("Your file has been uploaded !")
-            st.write("CAUTION: DO NOT select the option 'nan', it will cause a bug and you will have to refreash the page and insert the csv file again.")
+            #st.write("Your file has been uploaded !")
+            #st.write("CAUTION: DO NOT select the option 'nan', it will cause a bug and you will have to refreash the page and insert the csv file again.")
             Qst= st.selectbox("Choose the field you want to search by:",list(df.head()),key = "1")
             
             if file_uploaded is not None:
