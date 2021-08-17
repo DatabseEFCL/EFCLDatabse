1b6aa8b91ccd6b8c0ccd9e02eb48af2a1f3c00e6
Edmonton Federation of Community Leagues <87148095+DatabseEFCL@users.noreply.github.com>
2021-08-17 13:28:48 -0600

    Update EFCL.py

--- EFCL.py
+++ EFCL.py
@@ -11,7 +11,8 @@ st.write("Please upload your csv file, then select topic you want to search by."
 
 #Sidebar
 st.sidebar.subheader("Visualization Settings")
-file_uploaded= st.sidebar.file_uploader(label= "Upload your 'CLOG' csv file.", type= ['csv'], key='a') #upload clog csv file
+SideOption= st.sidebar.selectbox("Select the fields to use",options=("Map","Dataset")) #choose between Map or Data
+
 
 #chaching/saving CLOG data
 @st.cache(suppress_st_warning=True,allow_output_mutation=True)
@@ -19,15 +20,23 @@ def loadData(file_uploaded):
 
     st.write("Your file has been uploaded !")
     st.write("CAUTION: DO NOT select the option 'nan', it will cause a bug and you will have to refreash the page and insert the csv file again.")
-    gmaps = googlemaps.Client(key='AIzaSyAG23fb_Mhco1Xvlft4uhCqbU8h-d5-7w4')
+
     df = pd.read_csv(file_uploaded, encoding='unicode_escape')
     
-    return df,gmaps
+    return df
 
+#caching address 
+@st.cache(suppress_st_warning=True,allow_output_mutation=True)
+def map(file_uploaded2):
+     
+     df= pd.read_csv(file_uploaded2, encoding='unicode_escape')
+     st.write("There is no file uploaded")
+     st.write("Address file has been uploaded !")
+     return df
        
 
-def database(file_uploaded):
-        
+def database():
+        file_uploaded= st.sidebar.file_uploader(label= "Upload your 'CLOG' csv file.", type= ['csv'], key='a') #upload clog csv file
 
         #function to view the databse
         if file_uploaded:
@@ -37,6 +46,8 @@ def database(file_uploaded):
             st.write("CAUTION: DO NOT select the option 'nan', it will cause a bug and you will have to refreash the page and insert the csv file again.")
             Qst= st.selectbox("Choose the field you want to search by:",list(df.head(),key = "1")
             
+            if file_uploaded is not None:
+                
                         
                 if Qst == "Community League": #outout if the selected field is community league
                         Com=df['Community League'].sort_values().drop_duplicates()
@@ -62,21 +73,32 @@ def database(file_uploaded):
                         st.table(pd.concat([Program, Community], axis=1))
 
                 if Qst == "Address":#outout if the selected field is Delivery 
-                        gmaps = googlemaps.Client(key='AIzaSyAG23fb_Mhco1Xvlft4uhCqbU8h-d5-7w4')
-                        st.write("Address file has been uploaded !")
-                        League= st.selectbox("Select Community League :", list(df["Community League"]),key='z')
-                        StreetAd= df["Street Address"].loc[df['Community League']== League]
-                        #st.write(StreetAd)
-                        user_input= st.text_input("Please input user address.","",key="g")
-                        my_dist = gmaps.distance_matrix(user_input,StreetAd)['rows'][0]['elements'][0]["distance"]["text"] # api calling distance
-                        my_dur = gmaps.distance_matrix(user_input, StreetAd)['rows'][0]['elements'][0]["duration"]["text"]# api calling duration\
-                        st.write("The distance is ",my_dist) #destinaton output 
-                        st.write("The duration is ",my_dur) #duration output
+                        Directions(file_uploaded)
                 
                 else:
                         st.write("There is no file uploaded.")
+        
+
+def Directions(file_uploaded):
+
+        #csv file 
+        df= loadData(file_uploaded)
+        
+        # Requires API key
+        gmaps = googlemaps.Client(key='AIzaSyAG23fb_Mhco1Xvlft4uhCqbU8h-d5-7w4')
+
+        League= st.selectbox("Select Community League :", list(df["Community League"]),key='z')
+        StreetAd= df["Address"].loc[df['Community League']== League]
+        #st.write(StreetAd)
+        user_input= st.text_input("Please input user address.","",key="g")
+        my_dist = gmaps.distance_matrix(user_input,StreetAd)['rows'][0]['elements'][0]["distance"]["text"] # api calling distance
+        my_dur = gmaps.distance_matrix(user_input, StreetAd)['rows'][0]['elements'][0]["duration"]["text"]# api calling duration\
+        st.write("The distance is ",my_dist) #destinaton output 
+        st.write("The duration is ",my_dur) #duration output
+
+                   
+                
 
 if __name__== "__main__":
-        database(file_uploaded)
 
-        
+        database()
