445bfd3a9d62351f05821bdec9f48498646883cd
Edmonton Federation of Community Leagues <87148095+DatabseEFCL@users.noreply.github.com>
2021-08-23 12:35:50 -0600

    Update EFCL.py

--- EFCL.py
+++ EFCL.py
@@ -10,8 +10,8 @@ import googlemaps
 #st.title("EFCL CLOG Database Application")
 st.write("Please upload your csv file, then select topic you want to search by.")
 title_container = st.beta_container()
-col1,mid, col2 = st.beta_columns([1,1,20])
-image = Image.open('efcl.PNG')
+col1, col2 = st.beta_columns([1,20])
+image = Image.open('efcl.png')
 with title_container:
     with col1:
         st.image(image, width=64)
